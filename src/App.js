3d84bf5af04ef3ef9109bd642574e4be7e838878
kuroikyu <kuroikyu+github@gmail.com>
2018-05-17 18:55:49 +0100

    POC for fetching articles from feed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { articles: [] };
+
+  async componentDidMount() {
+    const newArticles = await this.fetchFeed(
+      'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ftechcrunch.com%2Ffeed%2F'
+    );
+
+    const articles = [...this.state.articles, ...newArticles];
+
+    this.setState({ articles });
+  }
+
+  fetchFeed = async uri => {
+    const rawData = await (await fetch(uri)).json();
+    const articles = rawData.items.map(item => ({ origin: rawData.feed, article: item }));
+
+    return articles;
+  };
+
   render() {
+    console.log(this.state);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section>
+        {this.state.articles.length > 0
+          ? this.state.articles.map(el => <li key={el.article.guid}>{el.article.title}</li>)
+          : ''}
+      </section>
     );
   }
 }
